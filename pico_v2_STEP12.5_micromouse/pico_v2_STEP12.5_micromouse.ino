3ed501e0b1d44907ef1b2cf5ef7596f0bbf2eff0
masatake aoki <aoki@rt-net.jp>
2025-10-05 11:00:30 +0900

    add step12.5

---
+++ pico_v2_STEP12.5_micromouse/pico_v2_STEP12.5_micromouse.ino
@@ -0,0 +1,63 @@
+// Copyright 2023 RT Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <FS.h>
+#include <WiFi.h>
+
+#include "SPIFFS.h"
+#include "adjust.h"
+#include "device.h"
+#include "fast.h"
+#include "map_manager.h"
+#include "misc.h"
+#include "parameter.h"
+#include "run.h"
+#include "search.h"
+#include "sensor.h"
+
+void setup()
+{
+  // put your setup code here, to run once:
+  deviceInit();
+  flashBegin();
+  paramRead();
+
+  motorDisable();
+  buzzerEnable(INC_FREQ);
+  delay(80);
+  buzzerDisable();
+
+  g_misc.mode_select = 1;
+
+
+
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  ledSet(g_misc.mode_select);
+  switch (switchGet()) {
+    case SW_RM:
+      g_misc.mode_select = g_misc.buttonInc(g_misc.mode_select, 15, 1);
+      break;
+    case SW_LM:
+      g_misc.buttonOk();
+      g_misc.modeExec(g_misc.mode_select);
+      break;
+  }
+  delay(1);
+}
---
+++ pico_v2_STEP13_micromouse/device.h
@@ -0,0 +1,50 @@
+// Copyright 2023 RT Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef DEVICE_H_
+#define DEVICE_H_
+
+#define SW_LM 1
+//#define SW_CM 2
+#define SW_RM 4
+
+#define LED0 42
+#define LED1 41
+#define LED2 15
+#define LED3 16
+
+#define BLED0 18
+#define BLED1 17
+
+#define SW_L 13
+#define SW_R 14
+
+#define SLED_F 2
+#define SLED_S 1
+
+#define BUZZER 40
+
+#define AD4 5
+#define AD3 4
+#define AD2 7
+#define AD1 6
+#define AD0 8
+
+#define MOTOR_EN 12
+#define CW_R 21
+#define CW_L 11
+#define PWM_R 45
+#define PWM_L 46
+
+#endif  // DEVICE_H_
