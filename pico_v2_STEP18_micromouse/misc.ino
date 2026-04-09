5f6d354b017e0c7a1309e1cbf37b69a861f83501
aoki-rt <aoki@rt.net.jp>
2026-04-09 09:10:13 +0900

    step18 add

---
+++ pico_v2_STEP18_micromouse/misc.ino
@@ -0,0 +1,315 @@
+// Copyright 2023 RT Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "fast.h"
+#include "misc.h"
+#include "sensor.h"
+
+MISC g_misc;
+
+const char* ssid_misc = "使用しているルータのSSID";
+const char* password_misc = "ルータのパスワード";
+
+uint32_t last_ota_time = 0;
+
+short MISC::buttonInc(short data, short limit, short limit_data)
+{
+  data++;
+  if (data > limit) {
+    data = limit_data;
+  }
+  buzzerEnable(INC_FREQ);
+  delay(30);
+  buzzerDisable();
+
+  return data;
+}
+
+void MISC::buttonOk(void)
+{
+  while (1) {
+    ledSet(0x0f);
+    delay(500);
+    if ((g_sensor.sen_fr.value + g_sensor.sen_fl.value) > 2000) {
+      break;
+    }
+    ledSet(0x00);
+    delay(500);
+    if ((g_sensor.sen_fr.value + g_sensor.sen_fl.value) > 2000) {
+      break;
+    }
+  }
+  ledSet(0x00);
+
+  buzzerEnable(DEC_FREQ);
+  delay(80);
+  buzzerEnable(INC_FREQ);
+  delay(80);
+  buzzerDisable();
+}
+
+void MISC::goalAppeal(void)  //ゴールしたことをアピールする
+{
+  unsigned char led_data;
+
+  int wtime = 100;
+
+  motorDisable();
+  mapWrite();
+
+  for (int j = 0; j < 10; j++) {
+    led_data = 1;
+    for (int i = 0; i < 4; i++) {
+      ledSet(led_data);
+      led_data <<= 1;
+      delay(wtime);
+    }
+    led_data = 8;
+    for (int i = 0; i < 4; i++) {
+      ledSet(led_data);
+      led_data >>= 1;
+      delay(wtime);
+    }
+    wtime -= 5;
+  }
+
+  delay(500);
+  motorEnable();
+}
+
+void MISC::modeExec(int mode)
+{
+  short max_speed_tmp;
+  motorEnable();
+  delay(1000);
+
+  switch (mode) {
+    case 1:
+      g_search.lefthand();
+      break;
+    case 2:  //足立法-low
+      max_speed_tmp = g_run.max_speed;
+      g_run.max_speed = g_run.search_speed;
+      g_map.positionInit();
+      g_search.adachi(g_map.goal_mx, g_map.goal_my, g_run.search_accel_low, g_run.search_speed_low);
+      g_run.rotate(right, 2);
+      g_map.nextDir(right);
+      g_map.nextDir(right);
+      g_misc.goalAppeal();
+      g_search.adachi(0, 0, g_run.search_accel_low, g_run.search_speed_low);
+      g_run.rotate(right, 2);
+      g_map.nextDir(right);
+      g_map.nextDir(right);
+      mapWrite();
+      g_run.max_speed = max_speed_tmp;
+      break;
+    case 3:  //足立法-normal
+      max_speed_tmp = g_run.max_speed;
+      g_run.max_speed = (g_run.search_speed + g_run.max_speed) / 2;
+      g_map.positionInit();
+      g_search.adachi(g_map.goal_mx, g_map.goal_my, g_run.search_accel, g_run.search_speed);
+      g_run.rotate(right, 2);
+      g_map.nextDir(right);
+      g_map.nextDir(right);
+      g_misc.goalAppeal();
+      g_search.adachi(0, 0, g_run.search_accel, g_run.search_speed);
+      g_run.rotate(right, 2);
+      g_map.nextDir(right);
+      g_map.nextDir(right);
+      mapWrite();
+      g_run.max_speed = max_speed_tmp;
+      break;
+    case 4:  //最短走行-normal
+      mapCopy();
+      g_map.positionInit();
+      g_fast.run(g_map.goal_mx, g_map.goal_my, g_run.search_accel, g_run.search_speed);
+      g_run.rotate(right, 2);
+      g_map.nextDir(right);
+      g_map.nextDir(right);
+      g_misc.goalAppeal();
+      g_fast.run(0, 0, g_run.search_accel, g_run.search_speed);
+      g_run.rotate(right, 2);
+      g_map.nextDir(right);
+      g_map.nextDir(right);
+      break;
+    case 5:  //最短走行-high
+      mapCopy();
+      g_map.positionInit();
+      g_fast.run(g_map.goal_mx, g_map.goal_my, g_run.search_accel_high, g_run.search_speed_high);
+      g_run.rotate(right, 2);
+      g_map.nextDir(right);
+      g_map.nextDir(right);
+      g_misc.goalAppeal();
+      g_fast.run(0, 0, g_run.search_accel, g_run.search_speed);
+      g_run.rotate(right, 2);
+      g_map.nextDir(right);
+      g_map.nextDir(right);
+    case 6:  //足立法 sura-low
+      max_speed_tmp = g_run.max_speed;
+      g_run.max_speed = g_run.search_speed;
+      g_map.positionInit();
+      g_search.adachi_sura(
+        g_map.goal_mx, g_map.goal_my, g_run.search_accel_low, g_run.search_speed_low - 25);
+      g_run.rotate(right, 2);
+      g_map.nextDir(right);
+      g_map.nextDir(right);
+      g_misc.goalAppeal();
+      g_search.adachi_sura(0, 0, g_run.search_accel_low, g_run.search_speed_low - 25);
+      g_run.rotate(right, 2);
+      g_map.nextDir(right);
+      g_map.nextDir(right);
+      mapWrite();
+      g_run.max_speed = max_speed_tmp;
+      break;
+    case 7:  //足立法 sura-normal
+      max_speed_tmp = g_run.max_speed;
+      g_run.max_speed = (g_run.search_speed + g_run.max_speed) / 2;
+      g_map.positionInit();
+      g_search.adachi_sura(
+        g_map.goal_mx, g_map.goal_my, g_run.search_accel, g_run.search_speed - 25);
+      g_run.rotate(right, 2);
+      g_map.nextDir(right);
+      g_map.nextDir(right);
+      g_misc.goalAppeal();
+      g_search.adachi_sura(0, 0, g_run.search_accel, g_run.search_speed - 25);
+      g_run.rotate(right, 2);
+      g_map.nextDir(right);
+      g_map.nextDir(right);
+      mapWrite();
+      g_run.max_speed = max_speed_tmp;
+      break;
+    case 8:
+      mapCopy();
+      g_map.positionInit();
+      g_fast.runSura(g_map.goal_mx, g_map.goal_my, g_run.search_accel, g_run.search_speed - 25);
+      g_run.rotate(right, 2);
+      g_map.nextDir(right);
+      g_map.nextDir(right);
+      g_misc.goalAppeal();
+      g_fast.runSura(0, 0, g_run.search_accel, g_run.search_speed - 25);
+      g_run.rotate(right, 2);
+      g_map.nextDir(right);
+      g_map.nextDir(right);
+      break;
+    case 9:
+      mapCopy();
+      g_map.positionInit();
+      g_fast.runSura(g_map.goal_mx, g_map.goal_my, g_run.search_accel_high, g_run.search_speed_high - 25);
+      g_run.rotate(right, 2);
+      g_map.nextDir(right);
+      g_map.nextDir(right);
+      g_misc.goalAppeal();
+      g_fast.runSura(0, 0, g_run.search_accel, g_run.search_speed - 25);
+      g_run.rotate(right, 2);
+      g_map.nextDir(right);
+      g_map.nextDir(right);
+      break;
+    case 10:
+      mapCopy();
+      g_map.positionInit();
+      g_fast.runPatternSura(g_map.goal_mx, g_map.goal_my, g_run.search_accel, g_run.search_speed - 25);
+      g_run.rotate(right, 2);
+      g_map.nextDir(right);
+      g_map.nextDir(right);
+      g_misc.goalAppeal();
+      g_search.adachi_sura(0, 0, g_run.search_accel, g_run.search_speed - 25);
+      g_run.rotate(right, 2);
+      g_map.nextDir(right);
+      g_map.nextDir(right);    
+      break;
+    case 11:
+      mapCopy();
+      g_map.positionInit();
+      g_fast.runPatternSura(g_map.goal_mx, g_map.goal_my, g_run.search_accel_high, g_run.search_speed_high - 25);
+      g_run.rotate(right, 2);
+      g_map.nextDir(right);
+      g_map.nextDir(right);
+      g_misc.goalAppeal();
+      g_search.adachi_sura(0, 0, g_run.search_accel, g_run.search_speed - 25);
+      g_run.rotate(right, 2);
+      g_map.nextDir(right);
+      g_map.nextDir(right);    
+      break;
+    case 12:
+      break;
+    case 13:
+      g_fast.patternMake(g_map.goal_mx, g_map.goal_my);
+      for (int i = 0; i < 256; i++) {
+        Serial.print(g_fast.second_pattern[i++]);
+        Serial.print(" ");
+        if (g_fast.second_pattern[i] == 127) {
+          Serial.println(g_fast.second_pattern[i]);
+          break;
+        } else {
+          Serial.println(g_fast.second_pattern[i]);
+        }
+      }
+      break;
+    case 14:
+      motorDisable();
+      WiFi.mode(WIFI_STA);
+      WiFi.begin(ssid_misc, password_misc);
+      while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+        Serial.println("Connection Failed! Rebooting...");
+        delay(5000);
+        ESP.restart();
+      }
+      ArduinoOTA
+        .onStart([]() {
+          String type;
+          if (ArduinoOTA.getCommand() == U_FLASH) {
+            type = "sketch";
+          } else {  // U_SPIFFS
+            type = "filesystem";
+          }
+          // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+          Serial.println("Start updating " + type);
+        })
+        .onEnd([]() {
+          Serial.println("\nEnd");
+        })
+        .onProgress([](unsigned int progress, unsigned int total) {
+          if (millis() - last_ota_time > 500) {
+            Serial.printf("Progress: %u%%\n", (progress / (total / 100)));
+            last_ota_time = millis();
+          }
+        })
+        .onError([](ota_error_t error) {
+          Serial.printf("Error[%u]: ", error);
+          if (error == OTA_AUTH_ERROR) {
+            Serial.println("Auth Failed");
+          } else if (error == OTA_BEGIN_ERROR) {
+            Serial.println("Begin Failed");
+          } else if (error == OTA_CONNECT_ERROR) {
+            Serial.println("Connect Failed");
+          } else if (error == OTA_RECEIVE_ERROR) {
+            Serial.println("Receive Failed");
+          } else if (error == OTA_END_ERROR) {
+            Serial.println("End Failed");
+          }
+      });
+      ArduinoOTA.begin();
+      Serial.println("Ready");
+      Serial.print("IP address: ");
+      Serial.println(WiFi.localIP());
+      break;
+    case 15:
+      motorDisable();
+      g_adjust.menu();  //調整メニューに行く
+      break;
+    default:
+      break;
+  }
+  motorDisable();
+}
